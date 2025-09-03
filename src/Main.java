c43591fb58d420793e3752010ddd715d1debedda
Irene Díez <irenediez@live.com>
2025-09-03 21:19:44 +0200

    Modificación de prueba 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome! lalalalaland");
+        System.out.println("Hello and welcome!");
     }
 }
